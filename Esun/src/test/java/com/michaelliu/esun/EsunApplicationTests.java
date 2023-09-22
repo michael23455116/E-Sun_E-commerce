17c3667e3a1b4e6c93d48ff2fdff8c50fb209b39
Michael <michael23455116@gmail.com>
2023-09-22 19:12:19 +0800

    初始化Spring Boot project

---
+++ Esun/src/test/java/com/michaelliu/esun/EsunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelliu.esun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
